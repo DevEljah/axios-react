8940e37786850b29c84f0f0d82a2db24502e887e
Jahid Eljah <jahideljah@gmail.com>
2023-02-17 20:42:58 +0100

    get (auth [header] ) and post requests

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,12 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import axios from "axios";
+import authFetch from "./../final/axios/interceptors";
 // limit, if 429 wait for 15 min and try again
-const url = 'https://course-api.com/react-store-products';
+const url = "https://course-api.com/react-store-products";
 
 const FirstRequest = () => {
+  const fetchData = async () => {
+    try {
+      const res = await axios(url);
+      // console.log(res);
+      const data = res.data;
+      console.log(data);
+    } catch (error) {
+      console.log(error.res);
+    }
+  };
+
   useEffect(() => {
-    console.log('first axios request');
+    fetchData(); // invoked !
   }, []);
 
-  return <h2 className='text-center'>first request</h2>;
+  return <h2 className="text-center">first request</h2>;
 };
 export default FirstRequest;
--- src/examples/2-headers.js
+++ src/examples/2-headers.js
@@ -1,22 +1,39 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const url = 'https://icanhazdadjoke.com/';
+const url = "https://icanhazdadjoke.com/";
 // Accept : 'application/json'
 
 const Headers = () => {
-  const [joke, setJoke] = useState('random dad joke');
+  const [joke, setJoke] = useState("random dad joke");
 
   const fetchDadJoke = async () => {
-    console.log('fetch dad joke');
+    console.log("fetch dad joke");
+    try {
+      const { data } = await axios(url, {
+        // const data = res.data; // destructured!
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      console.log(data);
+      // console.log(data.joke);
+      setJoke(data.joke);
+    } catch (error) {
+      console.log(error.res);
+    }
   };
 
+  useEffect(() => {
+    fetchDadJoke();
+  }, []);
+
   return (
-    <section className='section text-center'>
-      <button className='btn' onClick={fetchDadJoke}>
+    <section className="section text-center">
+      <button className="btn" onClick={fetchDadJoke}>
         random joke
       </button>
-      <p className='dad-joke'>{joke}</p>
+      <p className="dad-joke">{joke}</p>
     </section>
   );
 };
